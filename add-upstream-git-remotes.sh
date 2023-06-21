770a68e57ebc4c3afb73768659b6a2126957205d
Marc Walter <walter.marc@outlook.com>
2023-06-21 22:04:23 +0200

    Add script to add upstream official Elm repositories

---
+++ add-upstream-git-remotes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Add git remote `upstream` where needed"
+
+cd packages/
+
+for repo in *; do
+    if [ -d "$repo" ]; then
+        echo;
+        echo "Checking package $repo"
+        cd $repo
+        desired="git@github.com:elm/$repo.git"
+        upstream=$(git remote get-url upstream)
+        if [ -z "$upstream" ]; then
+            echo "Will set upstream to $desired"
+            git remote add upstream "$desired"
+        else
+            echo "upstream was already set to $upstream"
+        fi
+        git fetch upstream
+        cd ..
+    fi
+done
+
+cd ..
